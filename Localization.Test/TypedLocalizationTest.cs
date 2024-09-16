36406d37a2c671957485185443108c5ce3b44d69
sidubsolutions-ado <adoservices@sidub.net>
2024-09-16 01:57:08 +0000

    Release v1.0.17

--- Localization.Test/TypedLocalizationTest.cs
+++ Localization.Test/TypedLocalizationTest.cs
@@ -1,3 +1,27 @@
+/*
+ * Sidub Platform - Localization
+ * Copyright (C) 2024 Sidub Inc.
+ * All rights reserved.
+ *
+ * This file is part of Sidub Platform - Localization (the "Product").
+ *
+ * The Product is dual-licensed under:
+ * 1. The GNU Affero General Public License version 3 (AGPLv3)
+ * 2. Sidub Inc.'s Proprietary Software License Agreement (PSLA)
+ *
+ * You may choose to use, redistribute, and/or modify the Product under
+ * the terms of either license.
+ *
+ * The Product is provided "AS IS" and "AS AVAILABLE," without any
+ * warranties or conditions of any kind, either express or implied, including
+ * but not limited to implied warranties or conditions of merchantability and
+ * fitness for a particular purpose. See the applicable license for more
+ * details.
+ *
+ * See the LICENSE.txt file for detailed license terms and conditions or
+ * visit https://sidub.ca/licensing for a copy of the license texts.
+ */
+
 using Localization.Test.Resources;
 using Microsoft.Extensions.DependencyInjection;
 using Sidub.Platform.Localization;
@@ -58,4 +82,5 @@ namespace Localization.Test
         }
 
     }
+
 }
--- Localization/ILocalizationResource.cs
+++ Localization/ILocalizationResource.cs
@@ -1,9 +1,25 @@
 ﻿/*
  * Sidub Platform - Localization
- * Copyright (C) 2024 Sidub Solutions. All rights reserved.
+ * Copyright (C) 2024 Sidub Inc.
+ * All rights reserved.
  *
- * Dual-licensed under the AGPL v3 or a proprietary license. For details, see
- * https://sidub.ca/licensing or the LICENSE.txt file.
+ * This file is part of Sidub Platform - Localization (the "Product").
+ *
+ * The Product is dual-licensed under:
+ * 1. The GNU Affero General Public License version 3 (AGPLv3)
+ * 2. Sidub Inc.'s Proprietary Software License Agreement (PSLA)
+ *
+ * You may choose to use, redistribute, and/or modify the Product under
+ * the terms of either license.
+ *
+ * The Product is provided "AS IS" and "AS AVAILABLE," without any
+ * warranties or conditions of any kind, either express or implied, including
+ * but not limited to implied warranties or conditions of merchantability and
+ * fitness for a particular purpose. See the applicable license for more
+ * details.
+ *
+ * See the LICENSE.txt file for detailed license terms and conditions or
+ * visit https://sidub.ca/licensing for a copy of the license texts.
  */
 
 namespace Sidub.Platform.Localization
